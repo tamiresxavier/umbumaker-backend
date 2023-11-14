216be864740ec1c11dcd1439d3af47733831f7c2
tamiresxavier <tx977459@gmail.com>
2023-11-14 10:28:43 -0300

    unitary tests

---
+++ src/test/java/br/edu/ifpb/umbumaker/presentation/dto/AgendamentoDispositivoDtoTest.java
@@ -0,0 +1,39 @@
+package br.edu.ifpb.umbumaker.presentation.dto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.edu.ifpb.umbumaker.model.AgendamentoDispositivo;
+import br.edu.ifpb.umbumaker.model.enums.StatusObjeto;
+
+@SpringBootTest
+@Transactional
+public class AgendamentoDispositivoDtoTest {
+
+    @Test
+    public void testToModel() {
+        // Create sample input
+        LocalDate dataSolicitacao = LocalDate.now();
+        String email = "example@email.com";
+        LocalDate dataAgendamento = LocalDate.now().plusDays(7);
+        String descricao = "Sample Description";
+        String politicaDeAceite = "Sample Policy";
+        StatusObjeto status = StatusObjeto.INICIADO;
+
+        // Create an instance of the DTO
+        AgendamentoDispositivoDto agendamentoDto = new AgendamentoDispositivoDto(dataSolicitacao, email, dataAgendamento, descricao, politicaDeAceite, status);
+
+        // Test the conversion to the model
+        AgendamentoDispositivo agendamentoModel = agendamentoDto.toModel();
+
+        // Validate the output
+        assertEquals(dataSolicitacao, agendamentoModel.getDataSolicitacao());
+        assertEquals(email, agendamentoModel.getEmail());
+        // Check other properties similarly
+    }
+}
